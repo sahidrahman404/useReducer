387d24991b216a79a218bf8a1f1417cf10204ea4
Sahid Rahman <sahidrahman404@gmail.com>
2022-08-04 14:39:12 +0700

    change reducer function second parameter to receive a dispatch object with a type

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,24 @@
 import { useReducer } from "react";
 
-type Action = (currentState: { count: number }) => { count: number };
-const countReducer = (
-  state: { count: number },
-  action: Action | { count: number }
-) => ({
-  ...state,
-  ...(typeof action === "function" ? action(state) : action),
-});
+type ActionType = "INCREMENT" | "DECREMENT";
+type Action = { type: ActionType; step: number };
+const countReducer = (state: { count: number }, action: Action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return { count: state.count + action.step };
+    case "DECREMENT":
+      return { count: state.count - action.step };
+    default:
+      throw new Error(`Unsupported action type: ${action.type}`);
+  }
+};
 
 function Counter({ initialCount = 0, step = 1 }) {
-  const [state, setState] = useReducer(countReducer, {
+  const [state, dispatch] = useReducer(countReducer, {
     count: initialCount,
   });
-
   const { count } = state;
-  const increment = () =>
-    setState((currentState) => ({ count: currentState.count + step }));
+  const increment = () => dispatch({ type: "INCREMENT", step });
   return <button onClick={increment}>{count}</button>;
 }
 
